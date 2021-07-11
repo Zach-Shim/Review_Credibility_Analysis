7def22dd8214775da246ecaf659f3bb8df716b8b
Zach Shim <shimz2@uw.edu>
2021-07-10 17:47:13 -0700

    Improved formatting of index screen

--- django_site/fake_review_checker/catalog/urls.py
+++ django_site/fake_review_checker/catalog/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('search_link/', views.search_link, name='search_link'),
     path('about/', views.about, name='about'),
+    path('search_link/', views.search_link, name='search_link'),
     path('result/<str:product_ASIN>/', views.result, name = 'result'),
 ] 
  
